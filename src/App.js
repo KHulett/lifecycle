0e961e622461338456e3a74667d8de15f32501b2
K Hulett <kblocent@gmail.com>
2019-07-11 15:21:26 -0400

    App for js updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { loadPartialConfig } from '@babel/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+ componentDidMount() { //add in async
+  fetch('https://jsonplaceholder.typicode.com/users') // add in await then figure out what to do with .then's
+    .then(res => {
+      return res.json();
+    })
+      .then(data => {  
+      console.log(data)
+      this.setState({
+        isLoaded: true,
+        items:data,
+      })
+    })
 }
 
+render() {
+  var {isLoaded, items} = this.state;
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <div className="Names">
+          <ul>
+            {items.map(el => {
+              return (
+                <li key={el.id}>
+                  Name: {el.name} | UserName: {el.username} | {' '} <a href={`https.://${el.website}`}> Website </a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+}
 export default App;
